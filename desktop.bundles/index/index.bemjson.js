5c4bea344022ee6f6ff30d2116c6799ac15ecb1e
Mikhail Makarov <mikhail@makarov.us>
2016-01-11 05:48:32 +0300

    Захотелось покоммитить :)

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -13,35 +13,41 @@ module.exports = {
     scripts: [{ elem : 'js', url : 'index.min.js' }],
     content : [
         {
-            block : 'menu',
-            mods : { theme : 'orion' },
-            content : [
-                {
-                    block : 'menu-item',
-                    val : 1,
-                    content : 'ДОСТУП К ЦЕНАМ'
-                },
-                {
-                    block : 'menu-item',
-                    val : 1,
-                    content : 'КАТАЛОГ'
-                },
-                {
-                    block : 'menu-item',
-                    val : 1,
-                    content : 'РАСЧЕТ ЗАРАБОТКА ПО РЕГИОНУ'
-                },
-                {
-                    block : 'menu-item',
-                    val : 1,
-                    content : 'ОТЗЫВЫ'
-                },
-                {
-                    block : 'menu-item',
-                    val : 1,
-                    content : 'ВОПРОСЫ'
+            elem : 'menu_wrapper',
+            content : {
+                block : 'wrapper',
+                content : {
+                    block : 'menu',
+                    mods : { theme : 'orion' },
+                    content : [
+                        {
+                            block : 'menu-item',
+                            val : 1,
+                            content : 'ДОСТУП К ЦЕНАМ'
+                        },
+                        {
+                            block : 'menu-item',
+                            val : 1,
+                            content : 'КАТАЛОГ'
+                        },
+                        {
+                            block : 'menu-item',
+                            val : 1,
+                            content : 'РАСЧЕТ ЗАРАБОТКА ПО РЕГИОНУ'
+                        },
+                        {
+                            block : 'menu-item',
+                            val : 1,
+                            content : 'ОТЗЫВЫ'
+                        },
+                        {
+                            block : 'menu-item',
+                            val : 1,
+                            content : 'ВОПРОСЫ'
+                        }
+                    ]
                 }
-            ]
+            }
         },
         {
             block : 'first_screen',
@@ -96,7 +102,7 @@ module.exports = {
                     content : {
                         elem : 'text',
                         tag : 'h1',
-                        content : 'Приставки DTB-T2 <br> с возможностью наценки <br> до 100% в условиях кризиса!'
+                        content : 'Приставки DTB-T2 с&nbsp;возможностью наценки до&nbsp;100% в условиях кризиса!'
                     }
                 },
                 {
@@ -114,40 +120,313 @@ module.exports = {
                 },
                 {
                     block : 'wrapper',
-                    content : {
-                        block : 'boxes',
-                        content : [
-                            {
-                                elem : 'box_left',
-                                content : {
-                                    block : 'image',
-                                    url : '../../img/box-left.png'
+                    content : [
+                        {
+                            block : 'boxes',
+                            content : [
+                                {
+                                    elem : 'box_left',
+                                    content : {
+                                        block : 'image',
+                                        url : '../../img/box-left.png'
+                                    }
+                                },
+                                {
+                                    elem : 'box_right',
+                                    content : {
+                                        block : 'image',
+                                        url : '../../img/box-right.png'
+                                    }
+                                },
+                                {
+                                    elem : 'box_center',
+                                    content : {
+                                        block : 'image',
+                                        url : '../../img/box-center.png'
+                                    }
                                 }
-                            },
-                            {
-                                elem : 'box_right',
-                                content : {
-                                    block : 'image',
-                                    url : '../../img/box-right.png'
+                            ]
+                        },
+                        {
+                            block : 'promo',
+                            mix : { block : 'clearfix' },
+                            content : [
+                                {
+                                    elem : 'side',
+                                    elemMods : { align : 'left'},
+                                    content : [
+                                        {
+                                            elem : 'header',
+                                            content : 'Сегодня акция!'
+                                        },
+                                        {
+                                            elem : 'sub_header',
+                                            content : 'Первая партия <br> по <br> крупнооптовой цене!'
+                                        }
+                                    ]
+                                },
+                                {
+                                    elem : 'side',
+                                    elemMods : { align : 'right'},
+                                    content : [
+                                        {
+                                            elem : 'form_header',
+                                            content : 'Получите доступ ко всем оптовым ценам на сегодняшний день'
+                                        },
+                                        {
+                                            elem : 'form_action',
+                                            tag : 'form',
+                                            method : 'POST',
+                                            action : 'index.html',
+                                            content : [
+                                                {
+                                                    elem : 'inputs_wrapper',
+                                                    content : [
+                                                        {
+                                                            block : 'input',
+                                                            mods : { theme : 'orion', icon : 'user' },
+                                                            type : 'search',
+                                                            name : 'name',
+                                                            placeholder : 'Введите имя'
+                                                        },
+                                                        {
+                                                            block : 'input',
+                                                            mods : { theme : 'orion', icon : 'phone' },
+                                                            type : 'search',
+                                                            name : 'phone',
+                                                            placeholder : 'Введите телефон'
+                                                        },
+                                                        {
+                                                            block : 'input',
+                                                            mods : { theme : 'orion', icon : 'mail' },
+                                                            type : 'search',
+                                                            name : 'email',
+                                                            placeholder : 'Введите e-mail'
+                                                        }
+                                                    ]
+                                                },
+                                                {
+                                                    elem : 'button_wrapper',
+                                                    content : {
+                                                        block : 'button',
+                                                        mods : { theme : 'orion' },
+                                                        type : 'submit',
+                                                        name : 'form',
+                                                        val : '1',
+                                                        text : 'Получить доступ к оптовым ценам'
+                                                    }
+                                                },
+                                                {
+                                                    elem : 'checkbox_wrapper',
+                                                    content : {
+                                                        block : 'checkbox',
+                                                        mods : { theme : 'orion' },
+                                                        name : 'subscribe',
+                                                        text : 'Получать ежедневное обновление цен'
+                                                    }
+                                                }
+                                            ]
+                                        }
+                                    ]
                                 }
-                            },
-                            {
-                                elem : 'box_center',
-                                content : {
-                                    block : 'image',
-                                    url : '../../img/box-center.png'
-                                }
-                            }
-                        ]
-                    }
+                            ]
+                        }
+                    ]
                 }
             ]
         },
         {
-            block : 'content',
-            content : [
-                'content goes here'
-            ]
+            elem : 'second_screen_header',
+            tag : 'h2',
+            content : '9 бонусов, которые позволяют отлично зарабатывать на продаже DTB-T2!'
+        },
+        {
+            block : 'second_screen',
+            content : {
+                block : 'wrapper',
+                content : {
+                    block : 'bonuses',
+                    tag : 'ul',
+                    content: [
+                        {
+                            header : 'Заводская гарантия 1&nbsp;год',
+                            description : 'Если вам попался брак, мы вернем полную стоимость или заменим его со следующей поставкой.',
+                            icon : 'certificate'
+                        },
+                        {
+                            header : 'Доход от 50&nbsp;000&nbsp;руб до <br> 150&nbsp;000&nbsp;руб',
+                            description : 'Емкость рынка любого региона России не менее 5&nbsp;000 потенциальных покупателей!',
+                            icon : 'money'
+                        },
+                        {
+                            header : 'Возврат непроданного <br> товара',
+                            description : 'Вернем Вам деньги за неликвидный товар.',
+                            icon : 'money_back'
+                        },
+                        {
+                            header : 'Рекламные материалы <br> в подарок',
+                            description : 'Предоставляем листовки, плакаты и другие рекламные материалы бесплатно!',
+                            icon : 'gift'
+                        },
+                        {
+                            header : 'Доставка за наш счет!',
+                            description : 'Бесплатная доставка до транспортной компании или в любой регион России при заказе от&nbsp;10&nbsp;штук!!',
+                            icon : 'truck'
+                        },
+                        {
+                            header : 'Делимся выгодной системой продаж!',
+                            description : 'Каждому клиенту при первом заказе мы предоставляем 100% рабочую систему продаж, которая способна увеличить прибыль в&nbsp;2&nbsp;раза!',
+                            icon : 'graph'
+                        },
+                        {
+                            header : 'На наш товар накручивают больше',
+                            description : 'Наличие собственной логистической базы и собственное производство, позволяет нам предлагать минимальную цену на рынке.',
+                            icon : 'percent'
+                        },
+                        {
+                            header : 'Продвинем в интернете',
+                            description : 'Комплексная помощь в продвижении Вашего продукта в Яндекс и Google.',
+                            icon : 'globe'
+                        },
+                        {
+                            header : 'Сертифицированный товар',
+                            description : 'К каждой партии прикрепляем сертификат качества!',
+                            icon : 'roll'
+                        }
+                    ].map(function(item) {
+                        return {
+                            elem : 'bonus_item',
+                            tag : 'li',
+                            mods : { icon : item.icon },
+                            content : [
+                                {
+                                    elem : 'header',
+                                    tag : 'span',
+                                    content : item.header
+                                },
+                                {
+                                    elem : 'description',
+                                    tag : 'span',
+                                    content : item.description
+                                }
+                            ]
+                        };
+                    })
+                }
+            }
+        },
+        {
+            elem : 'bonus_button_wrapper',
+            content : {
+                block : 'button',
+                mods : { theme : 'orion' },
+                text : 'Получить отлаженную систему продаж'
+            }
+        },
+        {
+            block : 'screen_header',
+            content : {
+                elem : 'text',
+                content : 'КАТАЛОГ'
+            }
+        },
+        {
+            block : 'wrapper',
+            content : {
+                block : 'slider',
+                mix : { block : 'clearfix' },
+                content : [
+                    {
+                        elem : 'side',
+                        mods : { align : 'left' },
+                        content : {
+                            block : 'image',
+                            mix : { block : 'slider', elem : 'big_image' },
+                            url : '../../img/box-right.png'
+                        }
+                    },
+                    {
+                        elem : 'side',
+                        mods : { align : 'right' },
+                        content : [
+                            {
+                                elem : 'header',
+                                content : 'Цифровой телевизионный приемник'
+                            },
+                            {
+                                elem : 'header',
+                                mix : { block : 'slider', elem : 'editable_header' },
+                                content : 'ORION+&nbsp;RS&nbsp;T&nbsp;19&nbsp;HD'
+                            },
+                            {
+                                block : 'button',
+                                mix : { block : 'slider', elem : 'button' },
+                                mods : { theme : 'orion' },
+                                text : 'получить прайс-лист'
+                            },
+                            {
+                                elem : 'preview',
+                                content : 'Технические характеристики модели'
+                            },
+                            {
+                                elem : 'goods_list',
+                                content : [
+                                    {
+                                        name : 'rs-t19hd',
+                                        image : 'box-right.png',
+                                        state : 'active'
+                                    },
+                                    {
+                                        name : 'rs-t20hd',
+                                        image : 'box-center.png',
+                                    },
+                                    {
+                                        name : 'rs-t21hd',
+                                        image : 'box-left.png',
+                                    }
+                                ].map(function(item) {
+                                    return {
+                                        elem : 'good_item',
+                                        tag : 'li',
+                                        content : [
+                                            {
+                                                block : 'image',
+                                                mix : { block : 'slider', elem : 'icon' },
+                                                url : '../../img/' + item.image,
+                                                alt : item.name
+                                            },
+                                            {
+                                                elem : 'name',
+                                                mods : { active : ( item.state === 'active' ? 'yes' : '' ) },
+                                                content : item.name
+                                            }
+                                        ]
+                                    };
+                                })
+                            }
+                        ]
+                    }
+                ]
+            }
+        },
+        {
+            block : 'calc_screen',
+            content : {
+                elem : 'wrapper',
+                content : [
+                    {
+                        elem : 'header',
+                        content : 'Сколько Вы сможете зарабатывать на продаже в своем регионе?'
+                    }
+                ]
+            }
+        },
+        {
+            block : 'screen_header',
+            content : {
+                elem : 'text',
+                content : 'Посмотрите результаты наших клиентов'
+            }
         },
         {
             block : 'footer',
