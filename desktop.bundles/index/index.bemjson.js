50fe557c6a6ce53e06476accc96b9cfda582781f
Mikhail Makarov <mikhail@makarov.us>
2016-01-22 12:37:57 +0300

    Fixed footer edison

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -1296,8 +1296,8 @@ module.exports = {
                             {
                                 elem : 'header',
                                 tag : 'a',
-                                attrs : { target : '_blank', href : '//edison.bz/razrabotka_landing_page?utm_source=с_сайта_приставок&utm_medium=с_сайта_приставок&utm_campaign=с_сайта_приставок&utm_content=с_сайта_приставок&utm_term=с_сайта_приставок' },
-                                content : 'Разработка Landing Page'
+                                attrs : { target : '_blank', href : '//edison.bz/sozdanie_lending_peydzh?utm_source=с_сайта_приставок&utm_medium=с_сайта_приставок&utm_campaign=с_сайта_приставок&utm_content=с_сайта_приставок&utm_term=с_сайта_приставок' },
+                                content : 'Разработка лендинг пейдж'
                             },
                             {
                                 elem : 'logo'
